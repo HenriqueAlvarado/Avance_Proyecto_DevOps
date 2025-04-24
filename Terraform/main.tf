45e3fb468ae369bdd9c8d98cf592934048811039
HenriqueAlvarado <henrique.alvarado@gmail.com>
2025-04-23 20:35:53 -0600

    agrego puerto de entrada al security group

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -99,6 +99,12 @@ resource "aws_security_group" "web_sg" {
     security_groups = [aws_security_group.jump_sg.id]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
   egress {
     from_port   = 0
     to_port     = 0
